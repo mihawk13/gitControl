6d2780b817623a15bc13c2d6ea181773e1342b44
MiHawk <bbdev13@gmail.com>
2020-07-06 06:15:29 +0800

    Initial Commit

---
+++ gitControl/frmUtama.Designer.vb
@@ -0,0 +1,129 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmUtama
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmUtama))
+        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.richTxt = New System.Windows.Forms.RichTextBox()
+        Me.txtPesan = New System.Windows.Forms.TextBox()
+        Me.TimerGagal = New System.Windows.Forms.Timer(Me.components)
+        Me.TimerSukses = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'btnUpdate
+        '
+        Me.btnUpdate.BackColor = System.Drawing.Color.ForestGreen
+        Me.btnUpdate.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnUpdate.FlatAppearance.BorderSize = 0
+        Me.btnUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnUpdate.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnUpdate.ForeColor = System.Drawing.Color.White
+        Me.btnUpdate.Image = CType(resources.GetObject("btnUpdate.Image"), System.Drawing.Image)
+        Me.btnUpdate.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btnUpdate.Location = New System.Drawing.Point(12, 12)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(136, 44)
+        Me.btnUpdate.TabIndex = 0
+        Me.btnUpdate.Text = "UPDATE"
+        Me.btnUpdate.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage
+        Me.btnUpdate.UseVisualStyleBackColor = False
+        '
+        'btnReset
+        '
+        Me.btnReset.BackColor = System.Drawing.Color.Crimson
+        Me.btnReset.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnReset.FlatAppearance.BorderSize = 0
+        Me.btnReset.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnReset.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnReset.ForeColor = System.Drawing.Color.White
+        Me.btnReset.Image = CType(resources.GetObject("btnReset.Image"), System.Drawing.Image)
+        Me.btnReset.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btnReset.Location = New System.Drawing.Point(174, 12)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(136, 44)
+        Me.btnReset.TabIndex = 0
+        Me.btnReset.Text = " RESET"
+        Me.btnReset.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
+        Me.btnReset.UseVisualStyleBackColor = False
+        '
+        'richTxt
+        '
+        Me.richTxt.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.richTxt.Location = New System.Drawing.Point(12, 62)
+        Me.richTxt.Name = "richTxt"
+        Me.richTxt.ReadOnly = True
+        Me.richTxt.Size = New System.Drawing.Size(298, 172)
+        Me.richTxt.TabIndex = 1
+        Me.richTxt.Text = ""
+        '
+        'txtPesan
+        '
+        Me.txtPesan.BackColor = System.Drawing.Color.Gainsboro
+        Me.txtPesan.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPesan.ForeColor = System.Drawing.Color.White
+        Me.txtPesan.Location = New System.Drawing.Point(12, 240)
+        Me.txtPesan.Name = "txtPesan"
+        Me.txtPesan.ReadOnly = True
+        Me.txtPesan.Size = New System.Drawing.Size(298, 20)
+        Me.txtPesan.TabIndex = 2
+        Me.txtPesan.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'TimerGagal
+        '
+        Me.TimerGagal.Interval = 500
+        '
+        'TimerSukses
+        '
+        Me.TimerSukses.Interval = 500
+        '
+        'frmUtama
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.DarkSlateGray
+        Me.ClientSize = New System.Drawing.Size(323, 267)
+        Me.Controls.Add(Me.txtPesan)
+        Me.Controls.Add(Me.richTxt)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnUpdate)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "frmUtama"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Git Control"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnUpdate As System.Windows.Forms.Button
+    Friend WithEvents btnReset As System.Windows.Forms.Button
+    Friend WithEvents richTxt As System.Windows.Forms.RichTextBox
+    Friend WithEvents txtPesan As System.Windows.Forms.TextBox
+    Friend WithEvents TimerGagal As System.Windows.Forms.Timer
+    Friend WithEvents TimerSukses As System.Windows.Forms.Timer
+
+End Class
---
+++ gitControl/frmUtama.vb
@@ -0,0 +1,106 @@
+﻿Imports System.IO
+Imports System.Net
+Imports System.Web.Script.Serialization
+
+Public Class frmUtama
+    Private psi As ProcessStartInfo
+    Private cmd As Process
+    Private Delegate Sub InvokeWithString(ByVal text As String)
+
+    Private hasil As String = ""
+
+    Private Sub execute(ByVal arg As String)
+        Try
+            cmd.Kill()
+        Catch ex As Exception
+        End Try
+        hasil = ""
+        TimerGagal.Stop()
+        TimerSukses.Stop()
+        txtPesan.Text = ""
+        txtPesan.BackColor = Color.Gainsboro
+        richTxt.Clear()
+
+        psi = New ProcessStartInfo("git", arg)
+
+        Dim sysEncoding As System.Text.Encoding
+        System.Text.Encoding.GetEncoding(Globalization.CultureInfo.CurrentUICulture.TextInfo.OEMCodePage)
+        With psi
+            .UseShellExecute = False
+            .RedirectStandardError = True
+            .RedirectStandardOutput = True
+            .RedirectStandardInput = True
+            .CreateNoWindow = True
+            .StandardOutputEncoding = sysEncoding
+            .StandardErrorEncoding = sysEncoding
+        End With
+        cmd = New Process With {.StartInfo = psi, .EnableRaisingEvents = True}
+        AddHandler cmd.ErrorDataReceived, AddressOf AsyncDataReceived
+        AddHandler cmd.OutputDataReceived, AddressOf AsyncDataReceived
+        cmd.Start()
+        cmd.BeginOutputReadLine()
+        cmd.BeginErrorReadLine()
+    End Sub
+
+    Private Sub AsyncDataReceived(ByVal sender As Object, ByVal e As DataReceivedEventArgs)
+        Me.Invoke(New InvokeWithString(AddressOf SyncOutput), e.Data)
+    End Sub
+
+    Private Sub SyncOutput(ByVal text As String)
+        Try
+            If text.Contains("https://github.com") Then
+                'jangan tampilkan
+            Else
+                richTxt.AppendText(text & Environment.NewLine)
+                richTxt.ScrollToCaret()
+            End If
+        Catch ex As Exception
+
+        End Try
+        
+        hasil += text & Environment.NewLine
+
+        If hasil.Contains("file changed") Or hasil.Contains("insertion(+)") Then
+            txtPesan.Text = "Update Berhasil"
+            txtPesan.BackColor = Color.Lime
+            TimerGagal.Stop()
+            TimerSukses.Start()
+        ElseIf hasil.Contains("Already up to date.") Then
+            txtPesan.Text = "Belum ada update terbaru"
+            txtPesan.BackColor = Color.Blue
+        ElseIf hasil.Contains("HEAD is now at") Then
+            txtPesan.Text = "Reset Berhasil, Tekan UPDATE!"
+            txtPesan.BackColor = Color.Blue
+        Else 'If hasil.Contains("error:") Then
+            txtPesan.Text = "Update Gagal, Tekan Reset sebelum diupdate!"
+            txtPesan.BackColor = Color.Red
+            TimerGagal.Start()
+            TimerSukses.Stop()
+        End If
+    End Sub
+
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+        execute("pull")
+    End Sub
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        execute("reset --hard")
+    End Sub
+
+    Private Sub Timer_Tick(sender As Object, e As EventArgs) Handles TimerGagal.Tick
+        If txtPesan.BackColor = Color.Red Then
+            txtPesan.BackColor = Color.Crimson
+        ElseIf txtPesan.BackColor = Color.Crimson Then
+            txtPesan.BackColor = Color.Red
+        End If
+    End Sub
+
+    Private Sub TimerSukses_Tick(sender As Object, e As EventArgs) Handles TimerSukses.Tick
+        If txtPesan.BackColor = Color.Lime Then
+            txtPesan.BackColor = Color.LimeGreen
+        ElseIf txtPesan.BackColor = Color.LimeGreen Then
+            txtPesan.BackColor = Color.Lime
+        End If
+    End Sub
+End Class
+
